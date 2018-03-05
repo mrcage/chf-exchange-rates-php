1ca12c16b4856710e46f430a33d94593b58c032b
Nicolas Perrenoud <mail@nicu.ch>
2018-03-05 17:03:50 +0100

    Added main code class and composer file

---
+++ src/EzvExchangeRates.php
@@ -0,0 +1,63 @@
+<?php
+namespace MrCage\EzvExchangeRates;
+
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Cache;
+
+/**
+ * Retrieves exchange rates from Swiss Federal Customs Administration.
+ * 
+ * More information https://www.ezv.admin.ch/ezv/en/home/information-companies/declaring-goods/exchange-rates--sell-.html
+ */
+class EzvExchangeRates
+{
+    const EXCHANGE_RATE_XML_TODAY = 'http://www.pwebapps.ezv.admin.ch/apps/rates/rate/getxml?activeSearchType=today';
+    const EXCHANGE_RATE_XML_DATE = 'http://www.pwebapps.ezv.admin.ch/apps/rates/rate/getxml?activeSearchType=userDefinedDay&d=';
+
+    /**
+     * Retrieves the selected exchange rate for the given day (on weekends, the latest available value is used).
+     * 
+     * @param string $currency the currency, eg. EUR or USD (case-insensitive)
+     * @param Carbon\Carbon $date the date, or null to use the current day
+     * @return float the exchange rate compared to the Swiss Franc (CHF)
+     */
+    public static function getExchangeRate(string $currency, Carbon $date = null) : float {
+        if ($date == null || $date->isToday()) {
+            $url = self::EXCHANGE_RATE_XML_TODAY;
+        } else {
+            $url = self::EXCHANGE_RATE_XML_DATE . $date->format('Ymd');
+        }
+        // Cache results for a week, to avoid constant API calls for identical URLs
+        return Cache::remember('EzvExchangeRates:rate:'.$url, 10080, function () use ($url, $currency) {
+            $context  = stream_context_create(array('http' => array('header' => 'Accept: application/xml')));
+            $xml = file_get_contents($url, false, $context);
+            $xml = simplexml_load_string($xml);
+            foreach ($xml->devise as $devise) {
+                if ($devise['code'] == strtolower($currency)) {
+                    return (float)$devise->kurs;
+                }
+            }
+            throw new Exception('Unable to find current exchange rate for ' . $currency);
+        });
+    }
+
+    /**
+     * Gets a list of all available currencies.
+     * 
+     * @return array the list of currency codes (lowercase)
+     */
+    public static function listCurrencies() : array {
+        // Cache results for a week, to avoid constant API calls for identical URLs
+        return Cache::remember('EzvExchangeRates:currencies', 10080, function () {
+            $context  = stream_context_create(array('http' => array('header' => 'Accept: application/xml')));
+            $url = self::EXCHANGE_RATE_XML_TODAY;
+            $xml = file_get_contents($url, false, $context);
+            $xml = simplexml_load_string($xml);
+            $currencies = [];
+            foreach ($xml->devise as $devise) {
+                $currencies[] = $devise['code'];
+            }
+            return $currencies;
+        });
+    }
+}
